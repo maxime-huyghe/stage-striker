cd43c3a34508d210ab945ee5f3a1be54bc610808
Maxime Huyghe <maxhuy@laposte.net>
2022-05-24 17:54:12 +0200

    feature: cut corners on stage component

--- src/lib/Stage.svelte
+++ src/lib/Stage.svelte
@@ -10,7 +10,7 @@
   export let onClick: () => void = () => {};
 </script>
 
-<button class="m-2 relative shadow-md" on:click={onClick}>
+<button class="cut-corner m-2 relative shadow-md" on:click={onClick}>
   <div
     class="absolute z-10 w-full h-full"
     class:bg-black={selected !== "banned"}
@@ -19,11 +19,26 @@
     class:bg-opacity-50={selected === "no"}
     class:bg-opacity-70={selected === "banned"}
   >
-  <span
-    class="absolute z-20 bottom-0 left-0 w-full h-8 text-center text-lg bg-opacity-80 text-white bg-slate-800"
-  >
-    {stage.name}
-  </span>
+    <span
+      class="absolute z-20 bottom-0 left-0 w-full h-8 text-center text-lg bg-opacity-80 text-white bg-slate-800"
+    >
+      {stage.name}
+    </span>
   </div>
   <img src={stage.image} alt={stage.name} class="w-64" />
 </button>
+
+<style>
+  :root {
+    --cut-corner-size: 20px;
+  }
+  .cut-corner {
+    clip-path: polygon(
+      0 0,
+      calc(100% - var(--cut-corner-size)) 0,
+      100% var(--cut-corner-size),
+      100% 100%,
+      0 100%
+    );
+  }
+</style>
