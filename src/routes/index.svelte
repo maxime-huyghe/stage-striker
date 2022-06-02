b2802a1e29305267498e1e1bfede40736fe3680e
Maxime Huyghe <maxhuy@laposte.net>
2022-06-03 00:26:40 +0200

    refactor: change file structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,124 +1,17 @@
 <script context="module" lang="ts">
-  export const prerender = true;
+  import { DEFAULT_RULESET } from "$lib/types";
+  import { appUrl } from "$lib/urls";
+  import type { Load } from "./index";
+
+  export const load: Load = () => {
+    return {
+      status: 302,
+      redirect: appUrl(`/bo/${JSON.stringify(DEFAULT_RULESET)}`),
+    };
+  };
 </script>
 
-<script lang="ts">
-  import BoPicker from "$lib/BoPicker.svelte";
-  import StagePicker from "$lib/StagePicker.svelte";
-  import { GENTLEMAN_STAGES, NORMAL_STAGES } from "$lib/stages";
-  import { GENTLEMAN_TURNS, type Player, type SelectionResult } from "$lib/stageSelection";
-  import type { Phase } from "$lib/stageStriker";
-  import WinnerPicker from "$lib/WinnerPicker.svelte";
-
-  let phases: Phase[] = [
-    { name: "pickBO" },
-    { name: "gentleman", turns: [...GENTLEMAN_TURNS] },
-    { name: "rps" },
-    {
-      name: "firstPickBan",
-      turns: [
-        { ty: "ban", num: 3, player: "winner" },
-        { ty: "ban", num: 4, player: "loser" },
-        { ty: "pick", num: 1, player: "winner" },
-      ],
-    },
-    {
-      name: "pickWinner",
-    },
-    {
-      name: "restOfPickBansAndWinnerPicks",
-      turns: [
-        { ty: "ban", num: 3, player: "winner" },
-        { ty: "pick", num: 1, player: "loser" },
-      ],
-    },
-  ];
-
-  let bestOf: number;
-  let gentlemanStages: string[] = [];
-  let lastChosenStage: string;
-  let matchResults: Player[] = [];
-  let rpsWinner: Player;
-
-  $: lastWinner = (matchResults.length ? matchResults.at(-1) : 1) as Player;
-  $: requiredWins = Math.ceil(bestOf / 2);
-  $: player1Wins = matchResults.filter((p) => p === 1).length;
-  $: player2Wins = matchResults.filter((p) => p === 2).length;
-  $: someoneWon = player1Wins >= requiredWins || player2Wins >= requiredWins;
-
-  function advanceToNextPhase(): void {
-    if (phases[0].name === "restOfPickBansAndWinnerPicks" && !someoneWon) {
-      phases = [{ name: "pickWinner" }, ...phases];
-    } else {
-      console.debug("normal advance");
-      phases = phases.length ? phases.slice(1) : [];
-    }
-  }
-
-  function pickedBo(e: CustomEvent<number>) {
-    bestOf = e.detail;
-    advanceToNextPhase();
-  }
-
-  function pickedWinner(e: CustomEvent<Player>): void {
-    matchResults = [...matchResults, e.detail];
-    advanceToNextPhase();
-  }
-
-  function pickedRpsWinner(e: CustomEvent<Player>): void {
-    rpsWinner = e.detail;
-    advanceToNextPhase();
-  }
-
-  function pickedGentlemanStages(e: CustomEvent<SelectionResult>): void {
-    gentlemanStages = e.detail.picked;
-    advanceToNextPhase();
-  }
-
-  function pickedStage(e: CustomEvent<SelectionResult>): void {
-    lastChosenStage = e.detail.picked[0];
-    advanceToNextPhase();
-  }
-</script>
-
-<svelte:head>
-  <title>Stage Striker</title>
-</svelte:head>
-
-<main class="flex flex-col items-center">
-  {#if phases && !someoneWon}
-    {#if phases[0].name === "pickBO"}
-      <BoPicker on:choice={pickedBo} />
-    {:else if phases[0].name === "gentleman"}
-      <StagePicker
-        stages={GENTLEMAN_STAGES}
-        gentlemans={true}
-        lastWinner={1}
-        turns={phases[0].turns}
-        on:selection={pickedGentlemanStages}
-      />
-    {:else if phases[0].name === "rps"}
-      <WinnerPicker rpsMode={true} on:winner={pickedRpsWinner} />
-    {:else if phases[0].name === "pickWinner"}
-      <WinnerPicker {lastChosenStage} on:winner={pickedWinner} />
-    {:else if phases[0].name === "firstPickBan"}
-      <StagePicker
-        stages={[...NORMAL_STAGES, ...gentlemanStages]}
-        lastWinner={rpsWinner}
-        turns={phases[0].turns}
-        on:selection={pickedStage}
-      />
-    {:else if phases[0].name === "restOfPickBansAndWinnerPicks"}
-      <StagePicker
-        stages={[...NORMAL_STAGES, ...gentlemanStages]}
-        {lastWinner}
-        turns={phases[0].turns}
-        on:selection={pickedStage}
-      />
-    {/if}
-  {:else}
-    <div class="text-2xl">
-      Congratulations, player {player1Wins > player2Wins ? 1 : 2}!
-    </div>
-  {/if}
-</main>
+<!--
+  @component
+  For now, this just redirects to the default ruleset.
+-->
